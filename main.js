17673d151493851c19ffa7546d7c0b8bc63b55a2
Gregory Sims <gregorys234@gmail.com>
2023-10-03 22:16:11 -0500

    app crash ...restart point

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function checkout() {
   // Create a Stripe payment intent
-  const stripe = Stripe('pk_test_51NvhR1BaLhkzaUYzjxxLzJhDhIBNRmS3CKQVTh0Jk0NbHUx7II6eO3bOr62vp9C3iQyGvsn9LR01Z9Rm99Uz7wUy00z7ebNb5uY');
+  const stripe = Stripe('YOUR_API_KEY');
   const paymentIntent = stripe.paymentIntents.create({
     amount: 1000,
     currency: 'usd',
