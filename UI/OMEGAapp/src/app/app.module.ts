157f0f55c9625d692d057c03d24da82d7295bc73
Gregory Sims <gregorys234@gmail.com>
2024-01-29 21:34:46 -0600

    edit @JSON

---
+++ UI/OMEGAapp/src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
+
+
+imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    
+];
