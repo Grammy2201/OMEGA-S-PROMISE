140efa0c16c5666f6a562f3162b6c369d2835ed0
Gregory Sims <gregorys234@gmail.com>
2024-02-12 09:06:18 -0600

    edit for Routes

--- UI/OMEGAapp/src/app/app.module.ts
+++ UI/OMEGAapp/src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
+import { routes } from './app.routes';
 
 
 imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    RouterModule,
+    RouterModule.forRoot(routes)
     
 ];
+
+exports: [ RouterModule]
